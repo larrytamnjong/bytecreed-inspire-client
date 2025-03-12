08dcbfb82b8ddb37a5349acd239fb3682ab92b60
larrytamnjong <larrytabeh@gmail.com>
2025-03-12 14:10:42 +0100

    Update footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  // set the currenr year
+  // set the current year
   year: number = new Date().getFullYear();
 
   constructor() { }
