d2274718d84ea157c1f3894cf60bdd18bf7016ab
larrytamnjong <larrytabeh@gmail.com>
2025-03-18 16:45:43 +0100

    feat: Add PermissionActionEnum for school functionalities
    This commit introduces a new `PermissionActionEnum` to define permissions for various school functionalities. This enum provides a structured way to manage and control access to different features within the application, including dashboard, configuration, personnel, students, parents, academics, attendance, exams, results, reports, library, inventory, finance, boarding, transportation, and users. The enum also includes specific actions for managing users.

--- src/app/core/enums/permission-action-enum.ts
+++ src/app/core/enums/permission-action-enum.ts
@@ -1,3 +1,50 @@
-export enum PermissionActionEnum{
-    
-}
+export enum PermissionActionEnum {
+    // Dashboard
+    School_ViewDashboard = 1,
+  
+    // Configuration
+    School_ViewConfiguration = 2000,
+  
+    // Personnel
+    School_ViewPersonnel = 3000,
+  
+    // Students
+    School_ViewStudents = 4000,
+  
+    // Parents
+    School_ViewParents = 5000,
+  
+    // Academics
+    School_ViewAcademics = 6000,
+  
+    // Attendance
+    School_ViewAttendance = 7000,
+  
+    // Exams
+    School_ViewExams = 8000,
+  
+    // Results
+    School_ViewResults = 9000,
+  
+    // Report
+    School_ViewReports = 10000,
+  
+    // Library
+    School_ViewLibrary = 11000,
+  
+    // Inventory
+    School_ViewInventory = 12000,
+  
+    // Finance
+    School_ViewFinance = 13000,
+  
+    // Boarding
+    School_ViewBoarding = 14000,
+  
+    // Transportation
+    School_ViewTransportation = 15000,
+  
+    // Users
+    School_ViewUsers = 16000,
+    School_ManageUsers = 16001,
+  }
