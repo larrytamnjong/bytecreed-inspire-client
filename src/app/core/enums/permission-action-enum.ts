52e45a502e80918d81732bf8e13e570367ff8e30
larrytamnjong <larrytabeh@gmail.com>
2025-03-19 17:57:32 +0100

    feat: Enhance PermissionActionEnum with more actions
    This commit expands the `PermissionActionEnum` to include more granular permission actions for various school functionalities. Specifically, it adds actions for:
    
    -   Analytics (School_ViewAnalytics)
    -   Configuration management (School_ManageConfiguration)
    -   Student management (School_ManageStudents)
    -   Parent management (School_ManageParents)
    -   Result registration and history management (School_RegisterResults, School_ManageResultHistory)
    -   Report management (School_ManageReports)
    -   Subscription management (School_ManageSubscription)
    
    These additions provide more fine-grained control over user access and enable more specific permission assignments within the application.

--- src/app/core/enums/permission-action-enum.ts
+++ src/app/core/enums/permission-action-enum.ts
@@ -1,50 +1,58 @@
 export enum PermissionActionEnum {
-    // Dashboard
-    School_ViewDashboard = 1,
-  
-    // Configuration
-    School_ViewConfiguration = 2000,
-  
-    // Personnel
-    School_ViewPersonnel = 3000,
-  
-    // Students
-    School_ViewStudents = 4000,
-  
-    // Parents
-    School_ViewParents = 5000,
-  
-    // Academics
-    School_ViewAcademics = 6000,
-  
-    // Attendance
-    School_ViewAttendance = 7000,
-  
-    // Exams
-    School_ViewExams = 8000,
-  
-    // Results
-    School_ViewResults = 9000,
-  
-    // Report
-    School_ViewReports = 10000,
-  
-    // Library
-    School_ViewLibrary = 11000,
-  
-    // Inventory
-    School_ViewInventory = 12000,
-  
-    // Finance
-    School_ViewFinance = 13000,
-  
-    // Boarding
-    School_ViewBoarding = 14000,
-  
-    // Transportation
-    School_ViewTransportation = 15000,
-  
-    // Users
-    School_ViewUsers = 16000,
-    School_ManageUsers = 16001,
-  }
+  // Dashboard
+  School_ViewDashboard = 1,
+  School_ViewAnalytics = 2,
+
+  // Configuration
+  School_ViewConfiguration = 2000,
+  School_ManageConfiguration = 2001,
+
+  // Personnel
+  School_ViewPersonnel = 3000,
+
+  // Students
+  School_ViewStudents = 4000,
+  School_ManageStudents = 4001,
+
+  // Parents
+  School_ViewParents = 5000,
+  School_ManageParents = 5001,
+
+  // Academics
+  School_ViewAcademics = 6000,
+
+  // Attendance
+  School_ViewAttendance = 7000,
+
+  // Exams
+  School_ViewExams = 8000,
+
+  // Results
+  School_ViewResults = 9000,
+  School_RegisterResults = 9001,
+  School_ManageResultHistory = 9002,
+
+  // Report
+  School_ViewReports = 10000,
+  School_ManageReports = 10001,
+
+  // Library
+  School_ViewLibrary = 11000,
+
+  // Inventory
+  School_ViewInventory = 12000,
+
+  // Finance
+  School_ViewFinance = 13000,
+  School_ManageSubscription = 13001,
+
+  // Boarding
+  School_ViewBoarding = 14000,
+
+  // Transportation
+  School_ViewTransportation = 15000,
+
+  // Users
+  School_ViewUsers = 16000,
+  School_ManageUsers = 16001
+}
