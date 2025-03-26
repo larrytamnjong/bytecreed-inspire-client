edc7bb9a90be531adb89bc75401522eb44296d1b
larrytamnjong <larrytabeh@gmail.com>
2025-03-26 19:57:43 +0100

    feat: Update Institution component with breadcrumbs for better navigation

--- src/app/pages/general/institution/institution.component.ts
+++ src/app/pages/general/institution/institution.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-institution',
   templateUrl: './institution.component.html',
   styleUrl: './institution.component.scss'
 })
 export class InstitutionComponent {
+  breadCrumbItems!: Array<{}>;
 
+  ngOnInit(): void {
+    this.breadCrumbItems = [
+      { label: 'General' },
+      { label: 'Institution', active: true }
+    ];
+  }
 }
