55d51006f5121cd93510957656f84daffd002546
larrytamnjong <larrytabeh@gmail.com>
2025-04-12 12:54:04 +0100

    feat: Replace placeholder text with app-breadcrumbs component in student enrollment and registration pages; implement breadcrumb items in component logic

--- src/app/pages/school/students/student-enrollments/student-enrollments.component.ts
+++ src/app/pages/school/students/student-enrollments/student-enrollments.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/shared/base.component';
+import { Store } from '@ngrx/store';
+import { RootReducerState } from 'src/app/store';
 
 @Component({
   selector: 'app-student-enrollments',
   templateUrl: './student-enrollments.component.html',
   styleUrl: './student-enrollments.component.scss'
 })
-export class StudentEnrollmentsComponent {
+export class StudentEnrollmentsComponent extends BaseComponent implements OnInit {
+  breadCrumbItems!: Array<{}>;
 
+  constructor( 
+        protected override store: Store<{ data: RootReducerState }>, ) {
+        super(store);
+      }
+      ngOnInit(): void {
+        this.breadCrumbItems = [{label: 'Students'},{ label: 'Enrollment', active: true }];
+      }
 }
--- src/app/pages/school/students/student-registration/student-registration.component.ts
+++ src/app/pages/school/students/student-registration/student-registration.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/shared/base.component';
+import { Store } from '@ngrx/store';
+import { RootReducerState } from 'src/app/store';
 
 @Component({
   selector: 'app-student-registration',
   templateUrl: './student-registration.component.html',
   styleUrl: './student-registration.component.scss'
 })
-export class StudentRegistrationComponent {
+export class StudentRegistrationComponent extends BaseComponent implements OnInit {
+  breadCrumbItems!: Array<{}>;
 
+  constructor( 
+      protected override store: Store<{ data: RootReducerState }>, ) {
+      super(store);
+    }
+    ngOnInit(): void {
+      this.breadCrumbItems = [{label: 'Students'},{ label: 'Registration', active: true }];
+    }
 }
