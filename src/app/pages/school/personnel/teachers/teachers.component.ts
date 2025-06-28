13f8db5a1117b34a7ec45af5abcd31e342bf8971
larrytamnjong <larrytabeh@gmail.com>
2025-06-28 13:05:32 +0100

    feat: Add functionality to manage teacher subjects and classes with modals

--- src/app/pages/school/personnel/teachers/teachers.component.ts
+++ src/app/pages/school/personnel/teachers/teachers.component.ts
@@ -11,6 +11,9 @@ import { SimpleAlerts } from 'src/app/core/services/notifications/sweet-alerts';
 import { getErrorMessage } from 'src/app/core/helpers/error-filter';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { ClassService } from 'src/app/core/services/api/class.service';
+import { ClassSectionService } from 'src/app/core/services/api/class-section.service';
+import { SubjectService } from 'src/app/core/services/api/subject.service';
 @Component({
   selector: 'app-teachers',
   templateUrl: './teachers.component.html',
@@ -22,21 +25,37 @@ export class TeachersComponent extends BaseComponent implements OnInit{
  employees: any = [];
  teachers: any = [];
  teachersToDisplay: any = [];
+ selectedTeachers: any = [];
+
+ classes: any = [];
+ subjects: any = [];
+ sections: any = [];
 
   addTeacherForm!: UntypedFormGroup;
-  removeTeacherForm!: UntypedFormGroup;
+  teacherClassForm!: UntypedFormGroup;
+  teacherSubjectForm!: UntypedFormGroup;
   get fAddTeachersForm() { return this.addTeacherForm.controls; }
+  get fTeacherClassForm() { return this.teacherClassForm.controls; }
+  get fTeacherSubjectForm() { return this.teacherSubjectForm.controls; }
 
    headers: any = [
     { key: 'familyName', displayName: 'Family Name' },
-    { key: 'givenNames', displayName: 'Given Names'}
+    { key: 'givenNames', displayName: 'Given Names'},
+    { key: 'subjects', displayName: 'Subjects'},
+    { key: 'classes', displayName: 'Classes'},
+    { key: 'sections', displayName: 'Sections'}
   ];
 
   constructor(
     private modalService: NgbModal,
     private personnelService: PersonnelService,
     private teacherService: TeacherService,
+    private classService: ClassService,
+    private sectionService: ClassSectionService,
+    private subjectService: SubjectService,
     private addTeacherFormBuilder: UntypedFormBuilder,
+    private teacherClassFormBuilder: UntypedFormBuilder,
+    private teacherSubjectFormBuilder: UntypedFormBuilder,
     protected override store: Store<{ data: RootReducerState }>) {
       super(store);
     }
@@ -47,9 +66,24 @@ export class TeachersComponent extends BaseComponent implements OnInit{
       this.addTeacherForm = this.addTeacherFormBuilder.group({
           ids: [null, [Validators.required]],
       });
+
+      this.teacherClassForm = this.teacherClassFormBuilder.group({
+          teacherIds: [null, [Validators.required]],
+          classSectionIds: [null],
+          classIds: [null, [Validators.required]],
+      });
+
+      this.teacherSubjectForm = this.teacherSubjectFormBuilder.group({
+         subjectIds: [null, [Validators.required]],
+         teacherIds: [null, [Validators.required]],
+      });
+      
       this.getLookUps();
       this.getEmployees();
       this.getTeachers();
+      this.getClasses();
+      this.getSections();
+      this.getSubjects();
     }
 
     onSubmitAddTeacher() {
@@ -72,14 +106,95 @@ export class TeachersComponent extends BaseComponent implements OnInit{
         });
     }
 
+
+
+  onSubmitTeacherClasses(isDelete: boolean = false) {
+      this.submitted = true;
+      if (this.teacherClassForm.invalid) {
+        return;
+      }
+
+      this.toggleLoading();
+      if(isDelete){
+        this.teacherService.deleteTeacherClasses(this.teacherClassForm.value).pipe(
+          finalize(() => {this.toggleLoading();})).subscribe({
+            next: (response) => {
+              if(response.success){
+                this.getTeachers();
+                this.dismissModal();
+                SimpleAlerts.showSuccess();
+              }
+            },
+            error: (error) => {SimpleAlerts.showError(getErrorMessage(error));},
+        });
+      }else{
+        this.teacherService.addTeacherClasses(this.teacherClassForm.value).pipe(
+          finalize(() => {this.toggleLoading();})).subscribe({
+            next: (response) => {
+              if(response.success){
+                this.getTeachers();
+                this.dismissModal();
+                SimpleAlerts.showSuccess();
+              }
+            },
+            error: (error) => {SimpleAlerts.showError(getErrorMessage(error));},
+        });
+      }
+    }
+
+    onSubmitTeacherSubjects(isDelete: boolean = false) {
+      this.submitted = true;
+      if (this.teacherSubjectForm.invalid) {
+        return;
+      }
+      this.toggleLoading();
+
+      if(isDelete){
+        this.teacherService.removeTeacherSubjects(this.teacherSubjectForm.value).pipe(
+          finalize(() => {this.toggleLoading();})).subscribe({
+            next: (response) => {
+              if(response.success){
+                this.getTeachers();
+                this.dismissModal();
+                SimpleAlerts.showSuccess();
+              }
+            },
+            error: (error) => {SimpleAlerts.showError(getErrorMessage(error));},
+        });
+      }else{
+        this.teacherService.addTeacherSubjects(this.teacherSubjectForm.value).pipe(
+          finalize(() => {this.toggleLoading();})).subscribe({
+            next: (response) => {
+              if(response.success){
+                this.getTeachers();
+                this.dismissModal();
+                SimpleAlerts.showSuccess();
+              }
+            },
+            error: (error) => {SimpleAlerts.showError(getErrorMessage(error));},
+        });
+      }
+    }
+
   addModal(content: any) {
     this.submitted = false;
-    this.modalService.open(content, {...this.mdModalConfig, backdrop: 'static'});
+    this.modalService.open(content, {...this.lgModalConfig, backdrop: 'static'});
   }
 
-  editModal(content: any, teacher: any) {
+  teacherSubjectModal(content: any) {
+    this.teacherSubjectForm.patchValue({
+      teacherIds: this.selectedTeachers.map((teacher: any) => teacher.id),
+    });
     this.submitted = false;
-    this.modalService.open(content, {...this.mdModalConfig, backdrop: 'static'});
+    this.modalService.open(content, {...this.lgModalConfig, backdrop: 'static'});
+  }
+
+  teacherClassModal(content: any) {
+    this.teacherClassForm.patchValue({
+      teacherIds: this.selectedTeachers.map((teacher: any) => teacher.id),
+    });
+    this.submitted = false;
+    this.modalService.open(content, {...this.lgModalConfig, backdrop: 'static'});
   }
 
   deleteTeacher(teacher: any) {
@@ -106,12 +221,17 @@ export class TeachersComponent extends BaseComponent implements OnInit{
 
   resetForm() {
     this.addTeacherForm.reset();
+    this.teacherClassForm.reset();
+    this.teacherSubjectForm.reset();
+  }
+
+  selectedRowsChanged(event: any) {
+    this.selectedTeachers = event;
   }
 
   getEmployees() {
-      this.toggleLoading();
       this.personnelService.getEmployees().pipe(
-        finalize(() => {this.loading = false;})).subscribe({
+        finalize(() => {})).subscribe({
         next: (response) => {
           if(response.success){ this.employees = response.data;}
         },
@@ -122,7 +242,7 @@ export class TeachersComponent extends BaseComponent implements OnInit{
   getTeachers() {
       this.toggleLoading();
       this.teacherService.getTeachers().pipe(
-        finalize(() => {this.loading = false;})).subscribe({
+        finalize(() => {this.toggleLoading()})).subscribe({
         next: (response) => {
           if(response.success)
             { 
@@ -135,11 +255,17 @@ export class TeachersComponent extends BaseComponent implements OnInit{
     }
 
     setTeachersToDisplay(teachers: any[]): void {
-        this.teachersToDisplay = teachers.map((teacher) => {
+      this.teachersToDisplay = teachers.map((teacher) => {
+      const subjectsList = teacher.subjects ? teacher.subjects.map((s: any) => s.name).join(', ') : '';
+      const classesList = teacher.classes ? teacher.classes.map((c: any) => c.name).join(', ') : '';
+      const sections = teacher.sections ? teacher.sections.map((s: any) => s.name).join(', ') : '';
           return {
             id: teacher.id,
             familyName: teacher.personnel?.familyName || '#',
             givenNames: teacher.personnel?.givenNames || '#',
+            subjects: subjectsList, 
+            classes: classesList,
+            sections: sections
           };
         });
       }
@@ -147,5 +273,32 @@ export class TeachersComponent extends BaseComponent implements OnInit{
   dismissModal() {
       this.modalService.dismissAll();
       this.resetForm();
+    }
+
+  getSubjects(){
+    this.subjectService.getSubjects().subscribe({
+      next: (response) => {this.subjects = response.data;},
+      error: (error) => {}
+    });
+  }
+
+   getClasses() {
+      this.classService.getClasses().pipe(
+        finalize(() => {})).subscribe({
+        next: (response) => {
+          if(response.success){ this.classes = response.data;}
+        },
+        error: () => {},
+      });
+    }
+
+    getSections() {
+      this.sectionService.getClassSections().pipe(
+        finalize(() => {})).subscribe({
+        next: (response) => {
+          if(response.success){ this.sections = response.data;}
+        },
+        error: () => {},
+      });
     }
 }
