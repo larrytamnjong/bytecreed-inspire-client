17556033b55f8bb10c425f96c3883a6ac085d549
larrytamnjong <larrytabeh@gmail.com>
2025-08-01 15:20:15 +0100

    Add result achieves

--- src/app/core/services/api/result.service.ts
+++ src/app/core/services/api/result.service.ts
@@ -3,11 +3,11 @@ import { Observable } from "rxjs";
 import { ApiHttpService } from "../common/base-api-http.service";
 import { ServiceResponse } from "../../Models/common/service-response";
 import { RequestHelper } from "../common/service-request-helper";
+import { HttpParams } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root'})
 export class ResultService {
   private readonly RESULT_CONTROLLER: string = "v1/results";
-
 
   constructor(private apiService: ApiHttpService) {}
 
@@ -21,6 +21,20 @@ export class ResultService {
 
     public deleteResults(data: any): Observable<ServiceResponse<any>> {
         return this.apiService.delete(this.RESULT_CONTROLLER,RequestHelper.createServiceRequest(data));
+    }
+
+     public calculatePeriodRankings(academicPeriodId: any): Observable<ServiceResponse<any>> {
+         let queryParams = new HttpParams();
+         queryParams = queryParams.set('academicPeriodId', academicPeriodId);
+
+        return this.apiService.put(`${this.RESULT_CONTROLLER}/period-rankings`, null, {params: queryParams});
+    }
+
+    public calculateYearlyRankings(academicYearId: any): Observable<ServiceResponse<any>> {
+         let queryParams = new HttpParams();
+         queryParams = queryParams.set('academicYearId', academicYearId);
+
+        return this.apiService.put(`${this.RESULT_CONTROLLER}/year-rankings`, null, {params: queryParams});
     }
  
 }
--- src/app/pages/school/results/result-history/result-history.component.ts
+++ src/app/pages/school/results/result-history/result-history.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/shared/base.component';
 import { Store } from '@ngrx/store';
 import { RootReducerState } from 'src/app/store';
+import { AcademicService } from 'src/app/core/services/api/academics.service';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { ResultService } from 'src/app/core/services/api/result.service';
+import { getErrorMessage } from 'src/app/core/helpers/error-filter';
+import { SimpleAlerts } from 'src/app/core/services/notifications/sweet-alerts';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-result-history',
@@ -11,12 +17,106 @@ import { RootReducerState } from 'src/app/store';
 export class ResultHistoryComponent extends BaseComponent implements OnInit {
   breadCrumbItems!: Array<{}>;
 
-  constructor( protected override store: Store<{ data: RootReducerState }>) {
+   academicPeriods: any = [];
+   academicYears: any = [];
+
+   filterForm!: UntypedFormGroup;
+
+   canPerformPeriodRankings: boolean = false;
+   canPerformYearlyRankings: boolean = false;
+   canEditResults: boolean = false;
+
+  constructor( 
+    private filterFormBuilder: UntypedFormBuilder,
+    private resultService: ResultService,
+    private academicService: AcademicService,
+    protected override store: Store<{ data: RootReducerState }>) {
         super(store);
   }
 
   ngOnInit(): void {
-    this.breadCrumbItems = [{ label: 'Results' }, { label: 'Achieve', active: true }];
+    this.breadCrumbItems = [{ label: 'Results' }, { label: 'Archive ', active: true }];
+
+     this.filterForm = this.filterFormBuilder.group({
+          academicYearId: [null],
+          academicPeriodId: [null]
+        });
+
+    this.filterForm.valueChanges.subscribe(() => {
+      this.canPerformPeriodRankings = this.filterForm.get("academicYearId")?.value != null && this.filterForm.get("academicPeriodId")?.value != null;
+      this.canPerformYearlyRankings = this.filterForm.get("academicYearId")?.value != null
+      this.canEditResults = this.filterForm.get("academicYearId")?.value != null && this.filterForm.get("academicPeriodId")?.value != null;
+    });
+
+
+    this.filterForm.get("academicYearId")?.valueChanges.subscribe((value) => {
+      if (value) {
+        this.clearChildFormProperties();
+        this.getAcademicPeriods(value);
+      } 
+    });
+
+     this.getAcademicYears();
   }
 
-}
+  calculateYearlyRankings(){
+      this.toggleLoading();
+      this.resultService.calculateYearlyRankings(this.filterForm.get("academicYearId")?.value).pipe(finalize(() => {this.toggleLoading()})).subscribe({
+        next: (response) => {
+          if(response.success)
+            {
+              SimpleAlerts.showSuccess();
+            }
+        },
+        error: (error) => {
+          SimpleAlerts.showError(getErrorMessage(error));
+        },
+      });
+    }
+
+  calculatePeriodRankings(){
+      this.toggleLoading();
+      this.resultService.calculatePeriodRankings(this.filterForm.get("academicPeriodId")?.value).pipe(finalize(() => {this.toggleLoading()})).subscribe({
+        next: (response) => {
+          if(response.success)
+            {
+              SimpleAlerts.showSuccess();
+            }
+        },
+        error: (error) => {
+          SimpleAlerts.showError(getErrorMessage(error));
+        },
+      });
+    }
+
+    getAcademicPeriods(academicYearId?: any) {
+    this.academicService.getAcademicPeriods(academicYearId).subscribe({
+      next: (response) => {
+        if(response.success){
+          this.academicPeriods = response.data;
+        }
+      },
+      error: (error) => {
+      }
+    })
+  }
+
+  getAcademicYears(){
+    this.academicService.getAcademicYears().subscribe({
+      next: (response) => {
+        if(response.success){
+          this.academicYears = response.data;
+        }
+      },
+      error: (error) => {
+      }
+    })
+  }
+
+  
+
+  clearChildFormProperties() {
+    this.filterForm.patchValue({academicPeriodId: null});
+  }
+
+ }
