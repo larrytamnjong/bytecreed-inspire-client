6f59ba882ad29234f3c818cc46786e1f28c3d568
larrytamnjong <larrytabeh@gmail.com>
2025-08-20 17:56:44 +0100

    Refactor look-up table enums for improved organization and readability; add PaymentStatus and PaymentMethods enums.

--- src/app/core/enums/look-up-table.ts
+++ src/app/core/enums/look-up-table.ts
@@ -1,20 +1,45 @@
 export enum LookUpTableEnum {
-    Sex = 1,
-    Locales = 2,
-    StudentStatus = 3,
-    ApplicationTypes = 4,
-    ActiveAndInactiveStatus = 5,
-    YesOrNoResponse = 6,
+  Sex = 1,
+  Locales = 2,
+  StudentStatus = 3,
+  ApplicationTypes = 4,
+  ActiveAndInactiveStatus = 5,
+  YesOrNoResponse = 6,
+  PaymentStatus = 7,
+  PaymentMethods = 8,
 }
 
-export enum SexEnum{
-    Male = 1,
-    Female = 2,
+export enum SexEnum {
+  Male = 1,
+  Female = 2,
 }
 
-export enum StudentStatusEnum{
-    Active = 1,
-    Graduated = 2,
-    DroppedOut = 3,
-    Expelled = 4
+export enum StudentStatusEnum {
+  Active = 1,
+  Graduated = 2,
+  DroppedOut = 3,
+  Expelled = 4,
+}
+
+export enum ActiveAndInactiveStatusEnum {
+  Active = 1,
+  Inactive = 0,
+}
+
+export enum YesOrNoResponseEnum {
+  Yes = 1,
+  No = 0,
+}
+
+export enum PaymentStatusEnum {
+  Processing = 1,
+  Completed = 2,
+  Cancelled = 3,
+  Rejected = 4,
+}
+
+export enum PaymentMethodsEnum {
+  Cash = 1,
+  MTNCameroonMobileMoney = 2,
+  OrangeCameroonMobileMoney = 3,
 }
